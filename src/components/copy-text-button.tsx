65728ef547539aff19d8d78f5fa2d1e2daab3914
chick307 <chick307@gmail.com>
2020-01-23 02:01:04 +0900

    Add stylesheets to CopyTextButton component

--- src/components/copy-text-button.tsx
+++ src/components/copy-text-button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ClipboardService } from '../services/clipboard-service';
+import styles from './copy-text-button.css';
 
 export type Props = {
     children?: React.ReactNode;
@@ -16,9 +17,9 @@ export const CopyTextButton = (props: Props) => {
     }, [data]);
 
     return <>
-        <div>
-            <span>{data}</span>
-            <button onClick={onButtonClick}>{children}</button>
+        <div className={styles.copyTextButton}>
+            <span className={styles.data}>{data}</span>
+            <button className={styles.button} onClick={onButtonClick}>{children}</button>
         </div>
     </>;
 };
