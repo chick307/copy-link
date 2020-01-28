91ee18b0adbc35a0373b2e434b34ee2010881321
chick307 <chick307@gmail.com>
2020-01-29 04:36:37 +0900

    Add MessageBubble component

---
+++ src/components/message-bubble.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from './message-bubble.css';
+
+export type Direction = 'top' | 'right' | 'bottom' | 'left';
+
+export type Props = {
+    children: React.ReactNode;
+    className?: string;
+    direction?: Direction;
+};
+
+export const MessageBubble = (props: Props) => {
+    const { children, className = '', direction } = props;
+
+    const directionClassName = direction == null ? '' : `direction-${direction}`;
+
+    return <>
+        <div className={`${className} ${styles.mssageBubble}`}>
+            <div className={`${styles[directionClassName]} ${styles.message}`}>
+                {children}
+            </div>
+        </div>
+    </>;
+};
+
+export default MessageBubble;
