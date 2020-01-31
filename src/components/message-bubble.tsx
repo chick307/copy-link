b311074e17b43b960967333f4ada0a4d014955b9
chick307 <chick307@gmail.com>
2020-02-01 02:48:29 +0900

    Add color property to MessageBubble component

--- src/components/message-bubble.tsx
+++ src/components/message-bubble.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 
 import styles from './message-bubble.css';
 
+export type Color = 'black' | 'red';
+
 export type Direction = 'top' | 'right' | 'bottom' | 'left';
 
 export type Props = {
     children: React.ReactNode;
     className?: string;
+    color?: Color;
     direction?: Direction;
 };
 
 export const MessageBubble = (props: Props) => {
-    const { children, className = '', direction } = props;
+    const { children, className = '', color = 'black', direction } = props;
 
+    const colorClassName = `color-${color}`;
     const directionClassName = direction == null ? '' : `direction-${direction}`;
 
     return <>
         <div className={className}>
-            <div className={`${styles[directionClassName]} ${styles.message}`}>
+            <div className={`${styles[colorClassName]} ${styles[directionClassName]} ${styles.message}`}>
                 {children}
             </div>
         </div>
