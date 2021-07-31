49111acf83871210e2898d0d380f995f3fcd393a
chick307 <chick307@gmail.com>
2021-08-01 07:12:20 +0900

    Update webpack configuration files

--- webpack.common.js
+++ webpack.common.js
@@ -3,51 +3,59 @@
 const path = require('path');
 
 const CopyPlugin = require('copy-webpack-plugin');
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    context: __dirname,
-    entry: {
-        popup: './src/entries/popup.tsx',
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                exportLocalsConvention: 'camelCase',
+module.exports = (options) => {
+    return {
+        context: __dirname,
+        devtool: options.devtool,
+        entry: {
+            popup: './src/entries/popup.tsx',
+        },
+        mode: options.mode,
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                ...(options.cssLoaderOptions ?? {}),
+                                modules: {
+                                    exportLocalsConvention: 'camelCase',
+                                    ...(options.cssLoaderOptions?.modules ?? {}),
+                                },
                             },
                         },
-                    },
-                ],
-            },
+                    ],
+                },
+            ],
+        },
+        output: {
+            filename: path.join('scripts', '[name].js'),
+            path: path.resolve(__dirname, 'build', options.outputPath),
+        },
+        plugins: [
+            new CopyPlugin({ patterns: [{ from: 'src/assets', to: 'assets' }] }),
+            new CopyPlugin({ patterns: [{ from: 'src/views', to: 'views' }] }),
+            new MiniCssExtractPlugin({ filename: path.join('styles', '[name].css') }),
+            new GenerateJsonPlugin('manifest.json', options.manifest, null, 2),
         ],
-    },
-    output: {
-        filename: path.join('scripts', '[name].js'),
-    },
-    plugins: [
-        new CopyPlugin({ patterns: [{ from: 'src/assets', to: 'assets' }] }),
-        new CopyPlugin({ patterns: [{ from: 'src/views', to: 'views' }] }),
-        new MiniCssExtractPlugin({ filename: path.join('styles', '[name].css') }),
-    ],
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+    };
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,49 +1,26 @@
 /* eslint sort-keys: 'error' */
 
-const path = require('path');
-
-const GenerateJsonPlugin = require('generate-json-webpack-plugin');
-
 const manifest = require('./manifest.js');
-const common = require('./webpack.common.js');
+const config = require('./webpack.common.js');
 
-module.exports = {
-    ...common,
+module.exports = config({
+    cssLoaderOptions: {
+        modules: {
+            localIdentName: '[local]__[path][name]',
+        },
+    },
     devtool: 'inline-source-map',
+    manifest: {
+        ...manifest,
+        /* eslint-disable @typescript-eslint/camelcase */
+        browser_action: {
+            ...manifest.browser_action,
+            default_title: `${manifest.browser_action.default_title} [DEV]`,
+        },
+        name: `${manifest.name} [DEV]`,
+        content_security_policy: `${manifest.content_security_policy}; script-src 'self' 'unsafe-eval'`,
+        /* eslint-enable @typescript-eslint/camelcase */
+    },
     mode: 'development',
-    module: {
-        ...common.module,
-        rules: common.module.rules.map((rule) => {
-            return {
-                ...rule,
-                use: rule.use.map((use) => {
-                    if (use.loader === 'css-loader') {
-                        return {
-                            ...use,
-                            options: {
-                                ...use.options,
-                                modules: {
-                                    localIdentName: '[local]__[path][name]',
-                                },
-                            },
-                        };
-                    }
-                    return use;
-                }),
-            };
-        }),
-    },
-    output: {
-        ...common.output,
-        path: path.resolve(__dirname, 'build', 'dev'),
-    },
-    plugins: [
-        ...common.plugins,
-        new GenerateJsonPlugin('manifest.json', {
-            ...manifest,
-            /* eslint-disable @typescript-eslint/camelcase */
-            content_security_policy: `${manifest.content_security_policy}; script-src 'self' 'unsafe-eval'`,
-            /* eslint-enable @typescript-eslint/camelcase */
-        }, null, 2),
-    ],
-};
+    outputPath: 'dev',
+});
